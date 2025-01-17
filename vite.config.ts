5148618629c16e1a4e08335148dc949767c9d295
LiZhen <3081085609@qq.com>
2025-01-17 16:34:59 +0800

    feat: 更改打包编译工具为 vite

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { createStyleImportPlugin } from 'vite-plugin-style-import';
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    plugins: [
+        react(),
+        createStyleImportPlugin({
+            libs: [
+                {
+                    libraryName: 'antd',
+                    esModule: true,
+                    resolveStyle: (name) => `antd/es/${name}/style/index.css`
+                }
+            ]
+        })
+    ]
+});
